f565c2b5225ce19cbbcbd194cf41022e69a92e28
Tirine <larsson.rasmus@gmail.com>
2022-02-10 16:47:25 +0200

    Added operators and tests, fixed some bugs.
    Signed-off-by: Tirine <larsson.rasmus@gmail.com>

--- launch/launch/substitutions/join_substitutions.py
+++ launch/launch/substitutions/join_substitutions.py
@@ -12,36 +12,113 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Module for the PathJoinSubstitution substitution."""
+"""Module for the JoinSubstitutions substitution."""
+from __future__ import annotations
 
-import os
-from typing import Iterable
+from math import floor
+
+from typing import List, Iterable
 from typing import Text
 
 from ..launch_context import LaunchContext
 from ..some_substitutions_type import SomeSubstitutionsType
 from ..substitution import Substitution
+from ..utilities import normalize_to_list_of_substitutions
+from ..substitutions.text_substitution import TextSubstitution
+from .substitution_failure import SubstitutionFailure
 
 
 class JoinSubstitutions(Substitution):
-    """Substitution that join other substitutions on evaluation."""
+    """
+    Substitution that join other substitutions on evaluation.
+    
+    Beware: This should not be used instead of PathJoinSubstitution for paths,
+    as that is platform independent.
+    """
 
-    def __init__(self, substitutions: Iterable[SomeSubstitutionsType], joinWith: Text = "") -> None:
-        """Create a PathJoinSubstitution."""
-        from ..utilities import normalize_to_list_of_substitutions
-        self.__substitutions = normalize_to_list_of_substitutions(substitutions)
-        self.__joinWith = joinWith
+    def __init__(self, substitutions: SomeSubstitutionsType = [], join_symbol: Text = "") -> None:
+        """Create a JoinSubstitutions."""
+        Substitution.__init__(self)
+        self.__join_symbol = join_symbol
+        self.__substitutions = []
+        self.extend(substitutions)
 
+    def __add__(self, substitutions: SomeSubstitutionsType) -> JoinSubstitutions:
+        return JoinSubstitutions(self.__substitutions, self.__join_symbol).extend(substitutions)
+
+    def __iadd__(self, substitutions: SomeSubstitutionsType) -> JoinSubstitutions:
+        return self + substitutions
+
+    def __radd__(self, substitutions: SomeSubstitutionsType) -> JoinSubstitutions:
+        return JoinSubstitutions(substitutions, self.__join_symbol).extend(self.__substitutions)
+    
+    def __nonzero__(self) -> bool:
+        return len(self.__substitutions) != 0
+
+    def __mul__(self, integer: int) -> JoinSubstitutions:
+        if integer < 0:
+            raise ValueError("Negative multiplications are not possible." +
+            " For Substitution: '{}'".format(self.describe()))
+        if floor(integer) != integer:
+            raise ValueError("Non-integer multiplications are not possible." +
+            " For Substitution: '{}'".format(self.describe()))
+        new_substitution = JoinSubstitutions(join_symbol=self.__join_symbol)
+        for i in range(integer):
+            new_substitution += self
+        return new_substitution
+
+    def __imul__(self, integer: int) -> JoinSubstitutions:
+        return self*integer
+
+    def __rmul__(self, integer: int) -> JoinSubstitutions:
+        return self*integer
+
+    def __eq__(self, other) -> bool:
+        if type(self) is type(other):
+            if self is other:
+                return True
+            if self.describe() == other.describe():
+                return True
+        return False
     @property
-    def substitutions(self) -> Iterable[Substitution]:
+    def list_of_substitutions(self) -> List[SomeSubstitutionsType]:
         """Getter for substitutions."""
         return self.__substitutions
 
+    @property
+    def join_symbol(self) -> Text:
+        """Getter for substitutions."""
+        return self.__join_symbol
+
+    def extend(self, substitutions: SomeSubstitutionsType) -> self:
+        """
+        Extend substitutions or list of substitutions to the existing ones.
+        If it's the list contains another list, a sub_list, the sub_list will be made into
+        a JoinSubstitution(sub_list)
+        """
+        try:
+            if isinstance(substitutions, List):
+                for sub in substitutions:
+                    if isinstance(sub, List):
+                        sub = JoinSubstitutions(sub)
+                    sub = normalize_to_list_of_substitutions(sub)
+                    self.__substitutions.extend(sub)
+            else:
+                self.__substitutions.extend(normalize_to_list_of_substitutions(substitutions))
+            return self
+        except TypeError:
+            raise SubstitutionFailure(f'Cannot join a "{type(substitutions).__name__}" object as substitution.')
+
+    def set_join_symbol(self, symbol: Text) -> None:
+        self.__join_symbol = symbol
+
     def describe(self) -> Text:
         """Return a description of this substitution as a string."""
-        return "JoinSubstitutions('{}')".format(' + '.join([s.describe() for s in self.substitutions]))
+        return "JoinSubstitutions('{}')".format(
+            " + '{}' + ".format(self.__join_symbol)
+            .join([s.describe() for s in self.__substitutions]))
 
     def perform(self, context: LaunchContext) -> Text:
         """Perform the substitution by retrieving the local variable."""
         performed_substitutions = [sub.perform(context) for sub in self.__substitutions]
-        return self.__joinWith.join(performed_substitutions)
+        return self.__join_symbol.join(performed_substitutions)
---
+++ launch/test/launch/substitutions/test_join_substitutions.py
@@ -0,0 +1,168 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the JoinSubstitutions substitution class."""
+
+from launch.substitutions import JoinSubstitutions, SubstitutionFailure, PathJoinSubstitution
+from launch.substitution import Substitution
+from launch.actions import DeclareLaunchArgument
+from launch.launch_description import LaunchContext, LaunchDescription
+from launch.utilities import normalize_to_list_of_substitutions
+
+import pytest
+
+def test_empty_join_object():
+    text = "Test"
+    sub = JoinSubstitutions()
+    assert sub.perform(None) == ""
+
+def test_join_one_text_object():
+    text = "Test"
+    sub = JoinSubstitutions(text)
+    assert sub.perform(None) == text
+
+def test_join_faulty_object():
+    text = "Test"
+    faulty_subs = [
+        SubstitutionFailure(text),
+        DeclareLaunchArgument(text),
+        LaunchContext(),
+        LaunchDescription(),
+    ]
+    for sub in faulty_subs:
+        for i in range(10):
+            with pytest.raises(SubstitutionFailure) as ex:
+                JoinSubstitutions(sub)
+            sub = [text, sub]
+
+def test_join_multiple_text_object():
+    text = "Test"
+    text_list = [text]
+    result_text = text
+    for i in range(10):
+        sub = JoinSubstitutions(text_list)
+        assert sub.perform(None) == result_text
+        sub.extend(text_list)
+        for add_text in text_list:
+            result_text += add_text
+        text_list += text_list
+
+def test_join_text_object_with_join_symbol():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions([text,text,text], join_symbol)
+    assert sub.perform(None) == text + join_symbol + text + join_symbol + text
+    assert sub.join_symbol == join_symbol
+
+def test_join_text_object_and_change_join_symbol():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions([text,text,text], join_symbol)
+    assert sub.perform(None) == text + join_symbol + text + join_symbol + text
+    join_symbol = "_-_"
+    sub.set_join_symbol(join_symbol)
+    assert sub.perform(None) == text + join_symbol + text + join_symbol + text
+
+def test_join_multiple_text_and_list_object():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions([text,[text,text],text], join_symbol)
+    assert sub.perform(None) == text + join_symbol+  text+text  +join_symbol + text
+
+def test_add_join():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions(text, join_symbol)
+    sub = sub + text
+    text = text + join_symbol + text
+    assert sub.perform(None) == text
+
+def test_radd_join():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions(text, join_symbol)
+    sub = text + sub
+    text = text + join_symbol + text
+    assert sub.perform(None) == text
+
+def test_iadd_join():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions(text, join_symbol)
+    sub += sub
+    text += join_symbol + text
+    assert sub.perform(None) == text
+
+def test_mul_join():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions(text, join_symbol)
+    sub = sub*2
+    text = text + join_symbol + text
+    assert sub.perform(None) == text
+
+def test_imul_join():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions(text, join_symbol)
+    sub *= 2
+    text = text + join_symbol + text
+    assert sub.perform(None) == text
+
+def test_rmul_join():
+    text = "Test"
+    join_symbol = " + "
+    sub = JoinSubstitutions(text, join_symbol)
+    sub = 3*sub
+    text = text + join_symbol + text + join_symbol + text
+    assert sub.perform(None) == text
+
+def test_identity():
+    text = "Test"
+    join_symbol = " + "
+    sub1 = JoinSubstitutions([text,text])
+    sub2 = sub1
+    assert sub1 is sub2
+    sub2 += ""
+    assert sub1 is not sub2
+    sub1 = JoinSubstitutions([text,text], join_symbol)
+    sub2 = JoinSubstitutions([text,text], join_symbol)
+    assert sub1 is not sub2
+    sub2 = JoinSubstitutions([text,text])
+    assert sub1 is not sub2
+    sub2 = JoinSubstitutions(text+join_symbol, join_symbol)
+    assert sub1 is not sub2
+    sub2 = JoinSubstitutions(text+join_symbol+text, join_symbol)
+    assert sub1 is not sub2
+
+def test_equality():
+    text = "Test"
+    join_symbol = " + "
+    sub1 = JoinSubstitutions([text,text])
+    sub2 = sub1
+    assert sub1 == sub2
+    sub2 = JoinSubstitutions([text,text])
+    assert sub1 == sub2
+    sub2 += ""
+    assert sub1 != sub2
+    sub2 = JoinSubstitutions([text,text], join_symbol)
+    assert sub1 != sub2
+    sub2 = JoinSubstitutions(text+join_symbol, join_symbol)
+    assert sub1 != sub2
+    # Would be nice to be able to evaluate these equal, but for that a preform is needed or simplification of TextSub.
+    # sub2 = JoinSubstitutions(text+join_symbol+text, join_symbol)
+    # assert sub1 != sub2
+    # sub2 = sub1
+    # sub2 += ""
+    # assert sub1 != sub2
